3da6eb06d704f084b6d0780c35a5234e68d8746a
Medov Vladimir <vlmedov@mail.ru>
2023-12-12 23:32:23 +0300

    д/з №3 + seminar_3

---
+++ Homework_3/Tas1.swift
@@ -0,0 +1,24 @@
+// 1. Есть словарь с видами чая и их стоимостью. Есть очередь людей, 
+//    которые хотят заказать чай (можно представить её в виде массива 
+//    видов чая, которые хотят заказать).
+//    Необходимо последовательно выводить в консоль сколько заплатит покупатель 
+//   (необходимо вывести его номер по порядку, чай, который он заказал, и стоимость).
+
+enum teaType {
+  case earlGrey
+  case herbalTea
+  case yellowTea
+  case oolong
+}
+
+var teaPrices: [teaType: Int] = [.earlGrey: 5, .herbalTea: 4, .yellowTea: 3, .oolong: 6]
+
+let customerOrders: [teaType] = [.earlGrey, .oolong, .yellowTea, .herbalTea, .earlGrey]
+
+var totalCost: Int = 0
+for(index, tea) in customerOrders.enumerated() {
+  if let price = teaPrices[tea] {
+    totalCost += price
+    print("Cusomter #\(index + 1) will pay \(price) for \(tea)")
+  }
+}
